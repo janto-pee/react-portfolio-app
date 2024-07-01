c092bd6ba1bfed1d4969117a8cfeaff192cde5eb
aayo <aayo.software@gmail.com>
2024-07-01 13:09:36 -0400

    adding utilities to file ⚒️

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -2,11 +2,6 @@ import { HeartIcon } from "@heroicons/react/24/outline";
 import img1 from "../../assets/blog/blog-4.jpg";
 import img2 from "../../assets/blog/blog-5.jpg";
 import img3 from "../../assets/blog/blog-6.jpg";
-// import img2 from "../../assets/2.jpg";
-// import img3 from "../../assets/3.jpg";
-import img4 from "../../assets/4.jpg";
-import img5 from "../../assets/5.jpg";
-import img6 from "../../assets/6.jpg";
 
 const Blog = () => {
   const FeaturesList = [
